f88eea788d3ba5935bd786ffea07df012169f85f
m-iws <miki.iws@gmail.com>
2024-03-30 15:41:52 +0900

    sample_spec.rb

--- serverspec/spec/175.41.242.116/sample_spec.rb
+++ serverspec/spec/175.41.242.116/sample_spec.rb
@@ -1,6 +1,14 @@
 # rails_helperの読み込み
 require 'spec_helper'
 
+#80番ポートを指定
+listen_port = 80
+
+#指定されたポートをlistenしているか確認
+describe port(listen_port) do
+  it { should be_listening }
+end
+
 describe package('git') do
   it { should be_installed }
 end      
