c6d3fa2076c04755cb185922087fff8034d9ca0b
m-iws <miki.iws@gmail.com>
2024-03-30 15:15:05 +0900

    sample_spec.rb

--- serverspec/spec/175.41.242.116/sample_spec.rb
+++ serverspec/spec/175.41.242.116/sample_spec.rb
@@ -4,3 +4,7 @@ require 'spec_helper'
 describe package('git') do
   it { should be_installed }
 end      
+
+def os_platform_amazon?
+  Specinfra.backend.run_command('uname -r').stdout.include?("amzn1")
+end
